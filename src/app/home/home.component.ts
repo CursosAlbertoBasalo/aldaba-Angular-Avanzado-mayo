2843734ba94ece0425b09f52a34f0b92ec455a99
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-04 12:17:01 +0200

    feat: container

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Agency } from "../api/models/agency.interface";
+import { Trip } from "../api/models/trip.interface";
+import { HomeService } from "./home.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  agencies$: Observable<Agency[]>;
+  trips$: Observable<Trip[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly service: HomeService) {
+    this.agencies$ = service.getAgencies$();
+    this.trips$ = service.getTrips$();
   }
 
+  ngOnInit(): void {}
 }
---
+++ src/app/home/home.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Agency } from "../api/models/agency.interface";
+import { Trip } from "../api/models/trip.interface";
+import { AgenciesService } from "../api/services/agencies.service";
+import { TripsService } from "../api/services/trips.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HomeService {
+  constructor(private readonly agencies: AgenciesService, private readonly trips: TripsService) {}
+
+  getAgencies$(): Observable<Agency[]> {
+    return this.agencies.getAll$();
+  }
+  getTrips$(): Observable<Trip[]> {
+    return this.trips.getAll$();
+  }
+}
