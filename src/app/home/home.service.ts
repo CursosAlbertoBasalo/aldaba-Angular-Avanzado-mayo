00ecbf222925ebb3f7e42432552bfd96409c2fc2
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-04 12:29:05 +0200

    feat: delay and error

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { delay } from "rxjs/operators";
 import { Agency } from "../api/models/agency.interface";
 import { Trip } from "../api/models/trip.interface";
 import { AgenciesService } from "../api/services/agencies.service";
@@ -12,7 +13,9 @@ export class HomeService {
   constructor(private readonly agencies: AgenciesService, private readonly trips: TripsService) {}
 
   getAgencies$(): Observable<Agency[]> {
-    return this.agencies.getAll$();
+    // return this.agencies.getAll$();
+    return this.agencies.getAll$().pipe(delay(5000));
+    // return this.agencies.getError$();
   }
   getTrips$(): Observable<Trip[]> {
     return this.trips.getAll$();
