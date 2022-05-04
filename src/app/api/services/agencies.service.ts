0038f389e18e3bdf0038e45108df37696ff2f781
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-04 11:51:05 +0200

    feat: servicios

--- src/app/api/services/agencies.service.ts
+++ src/app/api/services/agencies.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Agency } from "../models/agency.interface";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AgenciesService {
+  private readonly apiUrl = environment.apiUrl + "/agencies";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll$(): Observable<Agency[]> {
+    console.log("getAll$", this.apiUrl);
+    return this.http.get<Agency[]>(this.apiUrl);
+  }
+  getError$(): Observable<Agency[]> {
+    return this.http.get<Agency[]>(this.apiUrl + "/error");
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [HeaderComponent, FooterComponent]
+  imports: [CommonModule, HttpClientModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
