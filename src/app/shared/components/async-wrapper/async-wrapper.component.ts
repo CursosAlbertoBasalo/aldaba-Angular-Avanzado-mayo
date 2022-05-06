b046f3d3c3c6fe0a50e1e438867ec6f178318aca
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-06 10:46:49 +0200

    feat: wrapper template

--- src/app/shared/components/async-wrapper/async-wrapper.component.ts
+++ src/app/shared/components/async-wrapper/async-wrapper.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+
+type Api = {
+  data?: unknown[];
+  error?: string;
+};
 
 @Component({
-  selector: 'app-async-wrapper',
-  templateUrl: './async-wrapper.component.html',
-  styleUrls: ['./async-wrapper.component.css']
+  selector: "app-async-wrapper",
+  templateUrl: "./async-wrapper.component.html",
+  styleUrls: ["./async-wrapper.component.css"],
 })
 export class AsyncWrapperComponent implements OnInit {
+  @Input() dataName = "";
+  api$: Observable<Api>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
